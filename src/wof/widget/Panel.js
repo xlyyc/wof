aa0907e9b58e68bb75f9dadcf700ee4b036b0365
xuliyu <xlyyc@163.com>
2013-10-01 08:43:25 +0800

    加入panel拖放支持

--- src/wof/widget/Panel.js
+++ src/wof/widget/Panel.js
@@ -17,6 +17,8 @@ wof.widget.Panel.prototype = {
 
     _overflow:null,
 
+    _initFlag:null,
+
     /**
      * get/set 属性方法定义
      */
@@ -34,6 +36,60 @@ wof.widget.Panel.prototype = {
 
     //选择实现
     beforeRender: function () {
+        if(this._initFlag==null){
+            var _this = this;
+            var timeFn = null;
+            this.getDomInstance().mousedown(function(event){
+                event.stopPropagation();
+                clearTimeout(timeFn);
+                timeFn = setTimeout(function(){
+                    _this.sendMessage('wof.widget.Panel_mousedown');
+                },250);
+            });
+            this.getDomInstance().dblclick(function(event){
+                event.stopPropagation();
+                clearTimeout(timeFn);
+                _this.sendMessage('wof.widget.Panel_dblclick');
+            });
+            this.getDomInstance().droppable({
+                snap:true,
+                accept:function(draggable){
+                    var b=false;
+                    var draggableObj = wof.util.ObjectManager.get(draggable.attr('oid'));
+                    if(draggableObj!=null){
+                        //不能接受FlowLayoutSection和FlowLayoutItem
+                        if(draggableObj.getClassName()!='wof.bizWidget.FlowLayoutSection'&&draggableObj.getClassName()!='wof.bizWidget.FlowLayoutItem'){
+                            //panel必须没有子节点
+                            if(_this.childNodes().length==0){
+                                b=true;
+                            }
+                        }
+                    }
+                    return b;
+                },
+                hoverClass: 'ui-state-hover',
+                drop:function(event,ui){
+                    event.stopPropagation();
+                    var obj = wof.util.ObjectManager.get(ui.draggable.attr('oid'));
+                    if(obj!=null){
+                        if(obj.getIsInside()==true){
+                            var obj = wof.util.ObjectManager.get(ui.draggable.attr('oid'));
+                            var node = eval('(new '+obj.getValue()+'()).createSelf('+_this.getWidth()+','+_this.getHeight()+');');
+                            node.appendTo(_this);
+                            _this.render();
+                        }else{
+                            console.log('todo .....');
+                           /* var sectionIndex = _this.parentNode().getIndex();
+                            _this.parentNode().parentNode().setActiveSectionIndex(sectionIndex);
+                            _this.parentNode().parentNode().setActiveItemRank({row:_this.getRow(),col:_this.getCol()});
+                            _this.sendMessage('wof.bizWidget.FlowLayoutItem_widgetDrop', {'widgetId':ui.draggable.attr('oid')});*/
+                        }
+
+                    }
+                }
+            });
+            this._initFlag = true;
+        }
         this.getDomInstance().css('overflow', '');
         this.getDomInstance().css('overflow-x', '');
         this.getDomInstance().css('overflow-y', '');
@@ -70,6 +126,17 @@ wof.widget.Panel.prototype = {
     //----------必须实现----------
     setData: function (data) {
         this.getOverflow(data.overflow);
+    },
+
+    //创建初始化的button
+    createSelf: function(width, height){
+        var node = new wof.widget.Panel();
+        node.setOverflow('auto');
+        node.setWidth(width);
+        node.setHeight(height);
+        node.setTop(0);
+        node.setLeft(0);
+        return node;
     }
 
 };
